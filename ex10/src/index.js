d8a6562e02429fb0c755ff9a5eb6b142cdd9edb3
Adnan <banjich@gmail.com>
2021-09-16 22:20:59 +0200

    Added file index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main (){
+    var myFirstName = "Adnan";
+    var myLastName = "Banjic";
+    var myHomeTown = "Kakanj";
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+
+console.log(main());
+module.exports = main;
